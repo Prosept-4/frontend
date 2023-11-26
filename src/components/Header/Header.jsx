e8de4fd14d23478f54ee1ceebdb69e72154c6af1
Богдан Исаченко <bogdan_isachenko@MacBook-Air-Bogdan.local>
2023-11-26 23:16:33 +0300

    feat: added header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,26 @@
 import logo from "../../images/prosept-logo.svg"
+import {Link, NavLink} from "react-router-dom";
 
 function Header() {
+  // const navigate = useNavigate();
+
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="надпись просепт и зеленое дерево"/>
+      <div className="header__nav">
+        <NavLink to="" className="header__button">
+          Разметка
+        </NavLink>
+        <NavLink to="" className="header__button">
+          Просмотр
+        </NavLink>
+        <NavLink to="" className="header__button">
+          Статистика
+        </NavLink>
+      </div>
+      <Link to="" className="header__button">
+        Выйти
+      </Link>
     </header>
   );
 }
