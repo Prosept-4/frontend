f001357ef7787e91fce5cd6e260508600a55c0e9
Krasnorutskiy Kirill <dullsharpz@gmail.com>
2023-12-10 00:25:22 +0300

    fix: protectedRoute

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import {Navigate} from "react-router-dom";
 
 function ProtectedRoute({ isLoggedIn, element: Component, ...props }) {
   return (
-    isLoggedIn ? <Component {...props} /> : <Navigate to='auth' replace />
+    isLoggedIn ? <Component {...props} /> : <Navigate to='/auth' replace />
   );
 }
 
